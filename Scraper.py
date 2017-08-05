611a6f845f7bcc72eb81116f9d4439b3a0f3e9e0
BuildTools <unconfigured@null.spigotmc.org>
2017-08-04 22:40:35 -0500

    Created fully functional web scraper and parsing mechanism with the ability to find all data contained within a certain element conforming to the target.

--- Scraper.py
+++ Scraper.py
@@ -10,7 +10,6 @@ class Target:
         length = len(self.elements)
         for i in range(length):
             index = -length + i
-            # print(path[index][0] == self.elements[0][0], path[index][0], self.elements[0][0], path[index][1] == self.elements[0][1], path[index][1], self.elements[0][1])
             if path[index][0] == self.elements[index][0] and (path[index][1] == self.elements[index][1] or self.elements[index][1] == ''):
                 continue
             else:
@@ -52,10 +51,23 @@ class Parser(HTMLParser):
         return self.occurrences
 
 
-def scrape(url):
-    data = requests.get(url).text
-    results = Parser(Target([['div', 'class="_sPg"']])).feed(data)
-    for r in results:
-        print(r)
+def scrape(url, header_payload, targets):
+    data = requests.get(url, headers=header_payload).text
+    results = []
 
-scrape("http://www.google.com/search?q=what+is+the+radius+of+a+circle")
+    for target in targets:
+        results += Parser(target).feed(data)
+
+    for result in results:
+        print(result)
+
+targets = [
+    Target([['div', 'class="_sPg"']]),
+    Target([['div', 'class="_XWk"']]),
+    Target([['span', 'class="_Tgc"']])
+]
+
+payload = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
+}
